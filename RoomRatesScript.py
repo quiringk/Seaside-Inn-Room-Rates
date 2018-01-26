30870b9d7b15e5a6d4583be7e673b3e19cd45ecf
quiringk <kevinquiring@Kevins-MacBook-Pro.local>
2018-01-26 00:45:56 -0800

    uncommented 3 hotels

--- RoomRatesScript.py
+++ RoomRatesScript.py
@@ -530,128 +530,128 @@ def checkForAlertWindow(browser):
         x = 1
     return
 
-# # ### Booking.com ####
-# #   
-# print("\n\n\n\n--- Booking ---\n\n")
-#    
-# browser.get("http://www.booking.com/searchresults.html?src=index&nflt=&ss_raw=seaside+&from_autocomplete=1&error_url=http%3A%2F%2Fwww.booking.com%2Findex.en-us.html%3Faid%3D337733%3Bsid%3Db698d280c4f42bc2672b035e51f569b4%3Bdcid%3D4%3B&aid=337733&dcid=4&sid=b698d280c4f42bc2672b035e51f569b4&si=ai%2Cco%2Cci%2Cre%2Cdi&ss=Seaside%2C+Oregon%2C+USA&checkin_monthday=25&checkin_year_month=2014-11&checkout_monthday=26&checkout_year_month=2014-11&interval_of_time=any&flex_checkin_year_month=any&no_rooms=1&group_adults=1&group_children=0&dest_type=city&dest_id=20105036&ac_pageview_id=e8f2106e931800ff&ac_position=0&ac_langcode=en&ac_suggestion_list_length=5")
-# time.sleep(1)
-#    
-# hotelWindow = webdriver.Chrome("/usr/local/bin/chromedriver")
-# hotelWindow.implicitly_wait(5)
-#    
-# hotellinks = browser.find_elements_by_xpath("//div[@class='sr_item_content']/h3/a")
-# for hotellink in hotellinks:
-#     print("\n\n--- " + hotellink.text + " ---\n\n")
-#     url = hotellink.get_attribute("href")
-#     hotelWindow.get(url)
-#     time.sleep(2)
-#     try:
-#         rooms = hotelWindow.find_elements_by_xpath("//a[@class='jqrt togglelink']")
-#         for room in rooms:
-#             print(room.text)
-#     except:
-#         pass
-#     try:
-#         prices = hotelWindow.find_elements_by_xpath("//strong[@class=' b-tooltip-with-price-breakdown-tracker rooms-table-room-price']")
-#         for price in prices:
-#             print(price.text)
-#     except:
-#         pass 
+ # ### Booking.com ####
+ #   
+ print("\n\n\n\n--- Booking ---\n\n")
     
-# ### Priceline ####
-#   
-# print("\n\n\n\n--- Priceline ---\n\n")
-#   
-# browser.get("http://www.priceline.com")
-# browser.find_element_by_xpath("//input[@name='cityName']").send_keys('Seaside, OR')
-#    
-# calendarLink = browser.find_element_by_xpath("//input[@name='checkInDate']").click()
-# fromCalendar = browser.find_element_by_xpath("//table[@class='ui-datepicker-calendar']")
-# print(fromCalendar)
-# # fromCalendar.click()
-# time.sleep(1)
-# rows = fromCalendar.find_elements_by_tag_name("tr")
-# i = 1;
-# for row in rows:
-#     if(i == 7):
-#         break
-#     days = rows[i].find_elements_by_tag_name("td")
-#     i += 1
-#     for day in days:
-#         if(day.text == "29"):
-#              day.find_element_by_tag_name("a").click()
-#              break
-#         else:
-#              continue
-#         break
-#     else:
-#         continue
-#     break
-#    
-# calendarLink = browser.find_element_by_xpath("//input[@name='checkOutDate']")
-# fromCalendar = browser.find_element_by_xpath("//table[@class='ui-datepicker-calendar']")
-# print(fromCalendar)
-# rows = fromCalendar.find_elements_by_tag_name("tr")
-# i = 1;
-# for row in rows:
-#     if(i == 7):
-#         break
-#     days = rows[i].find_elements_by_tag_name("td")
-#     i += 1
-#     for day in days:
-#         if(day.text == "30"):
-#              time.sleep(2)
-#              day.find_element_by_tag_name("a").click()
-#              time.sleep(2)
-#              break
-#         else:
-#              continue
-#         break
-#     else:
-#         continue
-#     break
-#    
-# hotelWindow = webdriver.Chrome("/usr/local/bin/chromedriver")
-# hotelWindow.implicitly_wait(10)
-#    
-# browser.find_element_by_xpath("//button[@id='hotel-btn-submit-retl']").click()
-# chooseButtons = browser.find_elements_by_xpath("//button[@class='listitem_choose']")
-# print(len(chooseButtons))
-# for chooseButton in chooseButtons:
-#     url = chooseButton.get_attribute('listitemexiturl')
-#     fullUrl = 'http://www.priceline.com' + url
-#     print(fullUrl)
-#     hotelWindow.get(fullUrl)
-#     hotelName = hotelWindow.find_element_by_xpath("//a[@class='gaHtlEvntTrk']")
-#     print("\n\n--- " + hotelName.text + " ---\n\n")
-#     rooms = hotelWindow.find_elements_by_xpath("//span[@class='rateInfoDesc']")
-#     for room in rooms:
-#         print(room.text)
-#     rates = hotelWindow.find_elements_by_xpath("//span[@class='rs_Display_rate_dollars']")
-#     for rate in rates:
-#         print(rate.text)
-# #      
-# # #### Hotels.com ####
-#   
-# print("\n\n\n\n--- Hotels.com ---\n\n")
-#   
-# browser.get("http://www.hotels.com/search.do?current-location=&arrivalDate=11%2F25%2F14&departureDate=11%2F26%2F14&searchParams.rooms.compact_occupancy_dropdown=compact_occupancy_1_1&rooms=1&searchParams.rooms%5B0%5D.numberOfAdults=1&children%5B0%5D=0&srsReport=HomePage%7CAutoS%7Ccity%7Cseaside+or%7C6%7C3%7C0%7C6%7C1%7C15%7C1483629&pageName=HomePage&searchParams.landmark=&resolvedLocation=CITY%3A1483629%3APROVIDED%3APROVIDED")
-# time.sleep(2)
-#    
-# hotelLinks = browser.find_elements_by_xpath("//a[@class='cta cta-hotel-select']")
-# print(len(hotelLinks))
-# for hotelLink in hotelLinks:
-#     url = hotelLink.get_attribute("href")
-#     hotelWindow.get(url)
-#     hotelName = hotelWindow.find_element_by_xpath("//h1[@class='fn org']")
-#     print("\n\n--- " + hotelName.text + " ---\n\n")
-#     rooms = hotelWindow.find_elements_by_xpath("//td[@class='room-type']/h3")
-#     for room in rooms:
-#         print(room.text)
-#     rates = hotelWindow.find_elements_by_xpath("//span[@class='current-price']/strong")
-#     for rate in rates:
-#         print(rate.text)
+ browser.get("http://www.booking.com/searchresults.html?src=index&nflt=&ss_raw=seaside+&from_autocomplete=1&error_url=http%3A%2F%2Fwww.booking.com%2Findex.en-us.html%3Faid%3D337733%3Bsid%3Db698d280c4f42bc2672b035e51f569b4%3Bdcid%3D4%3B&aid=337733&dcid=4&sid=b698d280c4f42bc2672b035e51f569b4&si=ai%2Cco%2Cci%2Cre%2Cdi&ss=Seaside%2C+Oregon%2C+USA&checkin_monthday=25&checkin_year_month=2014-11&checkout_monthday=26&checkout_year_month=2014-11&interval_of_time=any&flex_checkin_year_month=any&no_rooms=1&group_adults=1&group_children=0&dest_type=city&dest_id=20105036&ac_pageview_id=e8f2106e931800ff&ac_position=0&ac_langcode=en&ac_suggestion_list_length=5")
+ time.sleep(1)
+    
+ hotelWindow = webdriver.Chrome("/usr/local/bin/chromedriver")
+ hotelWindow.implicitly_wait(5)
+    
+ hotellinks = browser.find_elements_by_xpath("//div[@class='sr_item_content']/h3/a")
+ for hotellink in hotellinks:
+     print("\n\n--- " + hotellink.text + " ---\n\n")
+     url = hotellink.get_attribute("href")
+     hotelWindow.get(url)
+     time.sleep(2)
+     try:
+         rooms = hotelWindow.find_elements_by_xpath("//a[@class='jqrt togglelink']")
+         for room in rooms:
+             print(room.text)
+     except:
+         pass
+     try:
+         prices = hotelWindow.find_elements_by_xpath("//strong[@class=' b-tooltip-with-price-breakdown-tracker rooms-table-room-price']")
+         for price in prices:
+             print(price.text)
+     except:
+         pass 
+    
+ ### Priceline ####
+   
+ print("\n\n\n\n--- Priceline ---\n\n")
+   
+ browser.get("http://www.priceline.com")
+ browser.find_element_by_xpath("//input[@name='cityName']").send_keys('Seaside, OR')
+    
+ calendarLink = browser.find_element_by_xpath("//input[@name='checkInDate']").click()
+ fromCalendar = browser.find_element_by_xpath("//table[@class='ui-datepicker-calendar']")
+ print(fromCalendar)
+ # fromCalendar.click()
+ time.sleep(1)
+ rows = fromCalendar.find_elements_by_tag_name("tr")
+ i = 1;
+ for row in rows:
+     if(i == 7):
+         break
+     days = rows[i].find_elements_by_tag_name("td")
+     i += 1
+     for day in days:
+         if(day.text == "29"):
+              day.find_element_by_tag_name("a").click()
+              break
+         else:
+              continue
+         break
+     else:
+         continue
+     break
+    
+ calendarLink = browser.find_element_by_xpath("//input[@name='checkOutDate']")
+ fromCalendar = browser.find_element_by_xpath("//table[@class='ui-datepicker-calendar']")
+ print(fromCalendar)
+ rows = fromCalendar.find_elements_by_tag_name("tr")
+ i = 1;
+ for row in rows:
+     if(i == 7):
+         break
+     days = rows[i].find_elements_by_tag_name("td")
+     i += 1
+     for day in days:
+         if(day.text == "30"):
+              time.sleep(2)
+              day.find_element_by_tag_name("a").click()
+              time.sleep(2)
+              break
+         else:
+              continue
+         break
+     else:
+         continue
+     break
+    
+ hotelWindow = webdriver.Chrome("/usr/local/bin/chromedriver")
+ hotelWindow.implicitly_wait(10)
+    
+ browser.find_element_by_xpath("//button[@id='hotel-btn-submit-retl']").click()
+ chooseButtons = browser.find_elements_by_xpath("//button[@class='listitem_choose']")
+ print(len(chooseButtons))
+ for chooseButton in chooseButtons:
+     url = chooseButton.get_attribute('listitemexiturl')
+     fullUrl = 'http://www.priceline.com' + url
+     print(fullUrl)
+     hotelWindow.get(fullUrl)
+     hotelName = hotelWindow.find_element_by_xpath("//a[@class='gaHtlEvntTrk']")
+     print("\n\n--- " + hotelName.text + " ---\n\n")
+     rooms = hotelWindow.find_elements_by_xpath("//span[@class='rateInfoDesc']")
+     for room in rooms:
+         print(room.text)
+     rates = hotelWindow.find_elements_by_xpath("//span[@class='rs_Display_rate_dollars']")
+     for rate in rates:
+         print(rate.text)
+ #      
+ # #### Hotels.com ####
+   
+ print("\n\n\n\n--- Hotels.com ---\n\n")
+   
+ browser.get("http://www.hotels.com/search.do?current-location=&arrivalDate=11%2F25%2F14&departureDate=11%2F26%2F14&searchParams.rooms.compact_occupancy_dropdown=compact_occupancy_1_1&rooms=1&searchParams.rooms%5B0%5D.numberOfAdults=1&children%5B0%5D=0&srsReport=HomePage%7CAutoS%7Ccity%7Cseaside+or%7C6%7C3%7C0%7C6%7C1%7C15%7C1483629&pageName=HomePage&searchParams.landmark=&resolvedLocation=CITY%3A1483629%3APROVIDED%3APROVIDED")
+ time.sleep(2)
+    
+ hotelLinks = browser.find_elements_by_xpath("//a[@class='cta cta-hotel-select']")
+ print(len(hotelLinks))
+ for hotelLink in hotelLinks:
+     url = hotelLink.get_attribute("href")
+     hotelWindow.get(url)
+     hotelName = hotelWindow.find_element_by_xpath("//h1[@class='fn org']")
+     print("\n\n--- " + hotelName.text + " ---\n\n")
+     rooms = hotelWindow.find_elements_by_xpath("//td[@class='room-type']/h3")
+     for room in rooms:
+         print(room.text)
+     rates = hotelWindow.find_elements_by_xpath("//span[@class='current-price']/strong")
+     for rate in rates:
+         print(rate.text)
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -679,14 +679,14 @@ prevMonth2 = str((date2 - relativedelta(months=1)).month)
 prevYear1 = str((date1 - relativedelta(years=1)).year)
 prevYear2 = str((date2 - relativedelta(years=1)).year)
 
-# daysInMonth = calendar.monthrange(year, month)[1]
-# print(day)
-# print(month)
-# print(year)
-# print(daysInMonth)
-# useNextMonth = False
-# if(day == daysInMonth):
-#     useNextMonth = True
+ daysInMonth = calendar.monthrange(year, month)[1]
+ print(day)
+ print(month)
+ print(year)
+ print(daysInMonth)
+ useNextMonth = False
+ if(day == daysInMonth):
+     useNextMonth = True
 
 allRooms = []
 allRooms.append(getRiverTideKingRate(month1, day1, year1, month2, day2, year2))
